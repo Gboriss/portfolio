a0fbe5071068967ac8f695edf5e0173b42f01aae
Galina <galinaborissenko@gmail.com>
2020-12-08 21:30:41 +0300

    function hidenavmenu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 (() => {
     const filterConteiner = document.querySelector('.portfolio-filter')
-    const portfolioItemsContainer = document.querySelector('.portfolio-items')
     const portfolioItems = document.querySelectorAll('.portfolio-item')
-    console.log(portfolioItems)
+    
+    
     filterConteiner.addEventListener('click', (event) => {
+
         if(event.target.classList.contains('filter-item') && 
         !event.target.classList.contains('active')) {
             filterConteiner.querySelector('.active').classList.remove('outer-shadow', 'active')
@@ -11,7 +12,7 @@
             event.target.classList.add('outer-shadow', 'active')
             const target = event.target.getAttribute('data-target')
             portfolioItems.forEach((item) => {
-                if (target === item.getAttribute('data-category')) {
+                if (target === item.getAttribute('data-category') || target === 'all') {
                     item.classList.remove('hide')
                     item.classList.add('show')
                 } else {
@@ -22,4 +23,56 @@
             })
         }
     })
+
+    const sections = document.querySelectorAll('.section')
+
+    sections.forEach((section) => {
+        if(!section.classList.contains('active')) {
+            section.classList.add('hide')
+        }
+    })
+
+    const hamburgerBtn = document.querySelector('.hamburger-btn')
+    const navMenu = document.querySelector('.nav-menu')
+    const closeNavBtn = document.querySelector('.close-nav-menu')
+
+    hamburgerBtn.addEventListener('click', showNavMenu)
+
+    closeNavBtn.addEventListener('click', hideNavMenu)
+
+    function showNavMenu () {
+        navMenu.classList.add('open')
+        // bodyScrollingToggle()
+    }
+
+    function hideNavMenu () {
+        navMenu.classList.remove('open')
+        // bodyScrollingToggle()
+    }
+
+    document.addEventListener('click', (event) => {
+        if (event.target.classList.contains('link-item')) {
+            if (event.target.hash !== '') {
+                event.preventDefault()
+                const hash = event.target.hash 
+                console.log(hash)
+                document.querySelector('.section.active').classList.add('hide')
+                document.querySelector('.section.active').classList.remove('active')
+
+                document.querySelector(hash).classList.add('active')
+                document.querySelector(hash).classList.remove('hide')
+
+                navMenu.querySelector('.active').classList.add('outer-shadow', 'hover-in-shadow')
+                navMenu.querySelector('.active').classList.remove('active', 'inner-shadow')
+
+                event.target.classList.add('active', 'inner-shadow')
+                event.target.classList.remove('outer-shadow', 'hover-in-shadow')
+
+                hideNavMenu()
+
+            }
+        }
+    })
+
 })()
+
