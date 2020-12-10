03fcd110dd115000694d29abe2b4a8d2ac63458a
Galina <galinaborissenko@gmail.com>
2020-12-10 15:06:50 +0300

    mobile

--- js/main.js
+++ js/main.js
@@ -78,3 +78,9 @@ document.addEventListener('click', (event) => {
     }
 })
 
+window.addEventListener('load', () => {
+    document.querySelector('.preloader').classList.add('fade-out')
+    setTimeout(() => {
+        document.querySelector('.preloader').style.display='none'
+    }, 600)
+})
